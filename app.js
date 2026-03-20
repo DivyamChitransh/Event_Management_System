df16349e6c825d3dc68f89fae70a68950a9f1484
Divyam Chitransh <divyam@apeiros.com>
2026-03-21 00:47:11 +0530

    update for docker configs

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const connectDB = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 3306,
     });
 
     console.log("Connected to MySQL database");
